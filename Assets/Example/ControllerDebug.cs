985408e11b16de9c20ddcf383b95b852a07bb271
Shinya Tachihara <shinya@hololab.co.jp>
2019-07-05 04:35:06 +0900

    add: example scene

---
+++ Assets/Example/ControllerDebug.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class ControllerDebug : MonoBehaviour
+{
+    public Text textUI;
+    StringBuilder builder = new StringBuilder();
+
+    private void Start()
+    {
+        Application.logMessageReceived += (log, trace, obj) =>
+        {
+            textUI.text += $"{log}\r\n";
+        };
+
+        //var joysticks = Input.GetJoystickNames();
+        //foreach (var joystick in joysticks)
+        //    builder.AppendLine(joystick);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var rPos = $"L_POS:{InputTracking.GetLocalPosition(XRNode.LeftHand)}";
+        //var lPos = $"R_POS:{InputTracking.GetLocalPosition(XRNode.RightHand)}";
+
+        //builder.AppendLine(rPos);
+        //builder.AppendLine(lPos);
+
+
+    }
+}
---
+++ Assets/Example/SphereSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+    public GameObject sphere;
+
+    public void Create()
+    {
+        Instantiate(sphere, this.transform.position + new Vector3(Random.Range(-2f, .2f) , Random.Range(2f, .5f), Random.Range(-2f, .2f)), Quaternion.identity);
+    }
+}
