05918834af457f25d56a66e11295681bc2be1ee8
Shinya Tachihara <shinya@hololab.co.jp>
2019-07-05 04:34:48 +0900

    add: Oculus Extension

---
+++ Assets/MixedRealityToolkit.Oculus/OculusManager.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Input.UnityInput;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UInput = UnityEngine.Input;
+
+namespace Microsoft.MixedReality.Toolkit.Oculus.Input
+{
+    /// <summary>
+    /// Manages Oculus Quest Devices using unity's input system.
+    /// </summary>
+    [MixedRealityDataProvider(
+    typeof(IMixedRealityInputSystem),
+    SupportedPlatforms.Android,
+    "Oculus Device Manager")]
+    public class OculusManager : BaseInputDeviceManager
+    {
+        public OculusManager(
+            IMixedRealityServiceRegistrar registrar,
+            IMixedRealityInputSystem inputSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseMixedRealityProfile profile = null) : base(registrar, inputSystem, name, priority, profile) {}
+
+        private const float DeviceRefreshInterval = 3.0f;
+
+        protected static readonly Dictionary<string, GenericJoystickController> ActiveControllers = new Dictionary<string, GenericJoystickController>();
+
+        private float deviceRefreshTimer;
+        private string[] lastDeviceList;
+
+        #region Controller Utilities
+
+        /// <inheritdoc />
+        public override void Update()
+        {
+            deviceRefreshTimer += Time.unscaledDeltaTime;
+
+            if (deviceRefreshTimer >= DeviceRefreshInterval)
+            {
+                deviceRefreshTimer = 0.0f;
+                RefreshDevices();
+            }
+
+            foreach (var controller in ActiveControllers)
+            {
+                controller.Value?.UpdateController();
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Disable()
+        {
+            IMixedRealityInputSystem inputSystem = Service as IMixedRealityInputSystem;
+
+            foreach (var genericJoystick in ActiveControllers)
+            {
+                if (genericJoystick.Value != null)
+                {
+                    inputSystem?.RaiseSourceLost(genericJoystick.Value.InputSource, genericJoystick.Value);
+                }
+            }
+
+            ActiveControllers.Clear();
+        }
+
+        /// <inheritdoc/>
+        public override IMixedRealityController[] GetActiveControllers()
+        {
+            return ActiveControllers.Values.ToArray<IMixedRealityController>();
+        }
+
+        private void RefreshDevices()
+        {
+            IMixedRealityInputSystem inputSystem = Service as IMixedRealityInputSystem;
+
+            var joystickNames = UInput.GetJoystickNames();
+
+            if (joystickNames.Length <= 0) { return; }
+
+            if (lastDeviceList != null && joystickNames.Length == lastDeviceList.Length)
+            {
+                for (int i = 0; i < lastDeviceList.Length; i++)
+                {
+                    if (joystickNames[i].Equals(lastDeviceList[i])) { continue; }
+
+                    if (ActiveControllers.ContainsKey(lastDeviceList[i]))
+                    {
+                        var controller = GetOrAddController(lastDeviceList[i]);
+
+                        if (controller != null)
+                        {
+                            inputSystem?.RaiseSourceLost(controller.InputSource, controller);
+                        }
+
+                        ActiveControllers.Remove(lastDeviceList[i]);
+                    }
+                    else { Debug.Log("Controller does not contains kye"); }
+                }
+            }
+
+            for (var i = 0; i < joystickNames.Length; i++)
+            {
+                if (string.IsNullOrEmpty(joystickNames[i]))
+                {
+                    continue;
+                }
+
+                if (!ActiveControllers.ContainsKey(joystickNames[i]))
+                {
+                    var controller = GetOrAddController(joystickNames[i]);
+
+                    if (controller != null)
+                    {
+                        inputSystem?.RaiseSourceDetected(controller.InputSource, controller);
+                    }
+                }
+            }
+
+            lastDeviceList = joystickNames;
+        }
+
+        protected virtual GenericJoystickController GetOrAddController(string joystickName)
+        {
+            // If a device is already registered with the ID provided, just return it.
+            if (ActiveControllers.ContainsKey(joystickName))
+            {
+                var controller = ActiveControllers[joystickName];
+                Debug.Assert(controller != null);
+                return controller;
+            }
+
+            Handedness controllingHand;
+
+            if (joystickName.Contains("Left"))
+            {
+                controllingHand = Handedness.Left;
+            }
+            else if (joystickName.Contains("Right"))
+            {
+                controllingHand = Handedness.Right;
+            }
+            else
+            {
+                controllingHand = Handedness.None;
+            }
+
+            var currentControllerType = GetCurrentControllerType(joystickName);
+            Type controllerType;
+
+            if (currentControllerType == SupportedControllerType.OculusTouch)
+            {
+                controllerType = typeof(OculusTouchController);
+            }
+            else
+            {
+                return null;
+            }
+
+            IMixedRealityInputSystem inputSystem = Service as IMixedRealityInputSystem;
+
+            var pointers = RequestPointers(currentControllerType, controllingHand);
+            var inputSource = inputSystem?.RequestNewGenericInputSource($"{currentControllerType} Controller {controllingHand}", pointers, InputSourceType.Controller);
+            var detectedController = Activator.CreateInstance(controllerType, TrackingState.NotTracked, controllingHand, inputSource, null) as GenericJoystickController;
+
+            if (detectedController == null)
+            {
+                Debug.LogError($"Failed to create {controllerType.Name} controller");
+                return null;
+            }
+
+            if (!detectedController.SetupConfiguration(controllerType))
+            {
+                // Controller failed to be set up correctly.
+                Debug.LogError($"Failed to set up {controllerType.Name} controller");
+                // Return null so we don't raise the source detected.
+                return null;
+            }
+
+            for (int i = 0; i < detectedController.InputSource?.Pointers?.Length; i++)
+            {
+                detectedController.InputSource.Pointers[i].Controller = detectedController;
+            }
+
+            ActiveControllers.Add(joystickName, detectedController);
+            return detectedController;
+        }
+
+        protected virtual SupportedControllerType GetCurrentControllerType(string joystickName)
+        {
+            if (string.IsNullOrEmpty(joystickName) || !joystickName.Contains("Oculus"))
+            {
+                return 0;
+            }
+            else
+            {
+                return SupportedControllerType.OculusTouch;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/MixedRealityToolkit.Oculus/OculusTouchController.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Input.UnityInput;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Microsoft.MixedReality.Toolkit.Oculus.Input
+{
+    [MixedRealityController(
+    SupportedControllerType.OculusTouch,
+    new[] { Handedness.Left, Handedness.Right },
+    flags: MixedRealityControllerConfigurationFlags.UseCustomInteractionMappings)]
+    public class OculusTouchController: GenericJoystickController
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="trackingState"></param>
+        /// <param name="controllerHandedness"></param>
+        /// <param name="inputSource"></param>
+        /// <param name="interactions"></param>
+        public OculusTouchController(TrackingState trackingState, Handedness controllerHandedness, IMixedRealityInputSource inputSource = null, MixedRealityInteractionMapping[] interactions = null)
+            : base(trackingState, controllerHandedness, inputSource, interactions)
+        {
+            nodeType = controllerHandedness == Handedness.Left ? XRNode.LeftHand : XRNode.RightHand;
+        }
+
+        private XRNode nodeType;
+
+        /// <summary>
+        /// The current source state reading for this OpenVR Controller.
+        /// </summary>
+        public XRNodeState LastXrNodeStateReading { get; protected set; }
+
+        /// <summary>
+        /// Tracking states returned from the InputTracking state tracking manager
+        /// </summary>
+        private readonly List<XRNodeState> nodeStates = new List<XRNodeState>();
+
+        /// <inheritdoc />
+        public override MixedRealityInteractionMapping[] DefaultLeftHandedInteractions => new[]
+        {
+            new MixedRealityInteractionMapping(0, "Spatial Pointer", AxisType.SixDof, DeviceInputType.SpatialPointer, MixedRealityInputAction.None),
+            new MixedRealityInteractionMapping(1, "Axis1D.PrimaryIndexTrigger", AxisType.SingleAxis, DeviceInputType.Trigger, ControllerMappingLibrary.AXIS_9),
+            new MixedRealityInteractionMapping(4, "Axis1D.PrimaryIndexTrigger Press", AxisType.Digital, DeviceInputType.TriggerPress, ControllerMappingLibrary.AXIS_9),
+            new MixedRealityInteractionMapping(5, "Axis1D.PrimaryHandTrigger Press", AxisType.SingleAxis, DeviceInputType.Trigger, ControllerMappingLibrary.AXIS_11),
+            new MixedRealityInteractionMapping(6, "Axis2D.PrimaryThumbstick", AxisType.DualAxis, DeviceInputType.ThumbStick, ControllerMappingLibrary.AXIS_1, ControllerMappingLibrary.AXIS_2),
+            new MixedRealityInteractionMapping(10, "Button.Three Press", AxisType.Digital, DeviceInputType.ButtonPress, KeyCode.JoystickButton2),
+            new MixedRealityInteractionMapping(11, "Button.Four Press", AxisType.Digital, DeviceInputType.ButtonPress, KeyCode.JoystickButton3),
+        };
+
+        /// <inheritdoc />
+        public override MixedRealityInteractionMapping[] DefaultRightHandedInteractions => new[]
+        {
+            new MixedRealityInteractionMapping(0, "Spatial Pointer", AxisType.SixDof, DeviceInputType.SpatialPointer, MixedRealityInputAction.None),
+            new MixedRealityInteractionMapping(1, "Axis1D.SecondaryIndexTrigger", AxisType.SingleAxis, DeviceInputType.Trigger, ControllerMappingLibrary.AXIS_10),
+            new MixedRealityInteractionMapping(4, "Axis1D.SecondaryIndexTrigger Press", AxisType.Digital, DeviceInputType.TriggerPress, ControllerMappingLibrary.AXIS_10),
+            new MixedRealityInteractionMapping(5, "Axis1D.SecondaryHandTrigger Press", AxisType.SingleAxis, DeviceInputType.Trigger, ControllerMappingLibrary.AXIS_12),
+            new MixedRealityInteractionMapping(6, "Axis2D.SecondaryThumbstick", AxisType.DualAxis, DeviceInputType.ThumbStick, ControllerMappingLibrary.AXIS_4, ControllerMappingLibrary.AXIS_5),
+            new MixedRealityInteractionMapping(10, "Button.One Press", AxisType.Digital, DeviceInputType.ButtonPress, KeyCode.JoystickButton0),
+            new MixedRealityInteractionMapping(11, "Button.Two Press", AxisType.Digital, DeviceInputType.ButtonPress, KeyCode.JoystickButton1),
+        };
+
+        /// <inheritdoc />
+        public override void SetupDefaultInteractions(Handedness controllerHandedness)
+        {
+            AssignControllerMappings(controllerHandedness == Handedness.Left ? DefaultLeftHandedInteractions : DefaultRightHandedInteractions);
+        }
+
+        /// <inheritdoc />
+        public override void UpdateController()
+        {
+            if (!Enabled) { return; }
+
+            InputTracking.GetNodeStates(nodeStates);
+
+            for (int i = 0; i < nodeStates.Count; i++)
+            {
+                if (nodeStates[i].nodeType == nodeType)
+                {
+                    var xrNodeState = nodeStates[i];
+                    UpdateControllerData(xrNodeState);
+                    LastXrNodeStateReading = xrNodeState;
+                    break;
+                }
+            }
+
+            base.UpdateController();
+        }
+
+        /// <summary>
+        /// Update the "Controller" input from the device
+        /// </summary>
+        /// <param name="state"></param>
+        protected void UpdateControllerData(XRNodeState state)
+        {
+            var lastState = TrackingState;
+
+            LastControllerPose = CurrentControllerPose;
+
+            if (nodeType == XRNode.LeftHand || nodeType == XRNode.RightHand)
+            {
+                // The source is either a hand or a controller that supports pointing.
+                // We can now check for position and rotation.
+                IsPositionAvailable = state.TryGetPosition(out CurrentControllerPosition);
+                IsPositionApproximate = false;
+
+                IsRotationAvailable = state.TryGetRotation(out CurrentControllerRotation);
+
+                // Devices are considered tracked if we receive position OR rotation data from the sensors.
+                TrackingState = (IsPositionAvailable || IsRotationAvailable) ? TrackingState.Tracked : TrackingState.NotTracked;
+
+                CurrentControllerPosition = MixedRealityPlayspace.TransformPoint(CurrentControllerPosition);
+                CurrentControllerRotation = MixedRealityPlayspace.Rotation * CurrentControllerRotation;
+            }
+            else
+            {
+                // The input source does not support tracking.
+                TrackingState = TrackingState.NotApplicable;
+            }
+
+            CurrentControllerPose.Position = CurrentControllerPosition;
+            CurrentControllerPose.Rotation = CurrentControllerRotation;
+
+            // Raise input system events if it is enabled.
+            if (lastState != TrackingState)
+            {
+                InputSystem?.RaiseSourceTrackingStateChanged(InputSource, this, TrackingState);
+            }
+
+            if (TrackingState == TrackingState.Tracked && LastControllerPose != CurrentControllerPose)
+            {
+                if (IsPositionAvailable && IsRotationAvailable)
+                {
+                    InputSystem?.RaiseSourcePoseChanged(InputSource, this, CurrentControllerPose);
+                }
+                else if (IsPositionAvailable && !IsRotationAvailable)
+                {
+                    InputSystem?.RaiseSourcePositionChanged(InputSource, this, CurrentControllerPosition);
+                }
+                else if (!IsPositionAvailable && IsRotationAvailable)
+                {
+                    InputSystem?.RaiseSourceRotationChanged(InputSource, this, CurrentControllerRotation);
+                }
+            }
+        }
+    }
+}
